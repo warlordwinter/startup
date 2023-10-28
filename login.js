9f1f0221df23961328e1c13bd7369b4e6f5b646c
Wiley Welch <wileyjwelch1@gmail.com>
2023-10-28 14:46:50 -0600

    Here is the start of pmy JS

---
+++ login.js
@@ -0,0 +1,18 @@
+
+function login() {
+    // Get the input values
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    // You can perform login/authentication logic here
+    if (username && password) {
+        // Valid username and password, you can proceed
+        // For example, you can store the username in localStorage
+        localStorage.setItem('username', username);
+        // Redirect to another page
+        window.location.href = 'upload_station.html';
+    } else {
+        // Handle the case where login information is incomplete or incorrect
+        alert('Please enter a valid username and password.');
+    }
+}
