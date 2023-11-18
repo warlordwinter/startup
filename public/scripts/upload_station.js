c8bdfe65fc3673698adae082c2acc84f52bdfd58
Wiley Welch <wileyjwelch1@gmail.com>
2023-11-17 22:47:12 -0700

    fixed the backend for the mongos login and signup

--- public/scripts/upload_station.js
+++ public/scripts/upload_station.js
@@ -1,37 +1,57 @@
-// loadpdf.js
-document.getElementById("pdf-file-input").addEventListener('change', function(event) {
-    loadPDF();
-});
+// combined.js
 
-function loadPDF() {
-    const input = document.getElementById("pdf-file-input");
+// Function to send JSON data to the server
+async function sendToServer(data) {
+    try {
+        const response = await fetch('/upload-pdf', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ data }),
+        });
 
-    const file = input.files[0];
-    if (file) {
-        const reader = new FileReader();
-        reader.onload = function(event) {
-            const data = event.target.result;
-            sendToServer(data); // Call the function to store in MongoDB
-        };
-        reader.readAsArrayBuffer(file);
-    } else {
-        alert("Please select a PDF file.");
+        if (response.ok) {
+            const result = await response.json();
+            console.log('Server response:', result);
+        } else {
+            console.error('Error sending data to the server:', response.status);
+        }
+    } catch (error) {
+        console.error('Error:', error);
     }
 }
 
-function sendToServer(data) {
-    fetch('/upload-pdf', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ data }),
-    })
-    .then(response => response.json())
-    .then(result => {
-        console.log(result);
-    })
-    .catch(error => {
+// Function to get PDF data from the server
+async function getPDF() {
+    try {
+        const response = await fetch('/get-pdf', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            const result = await response.json();
+            console.log('PDF data from the server:', result);
+            return result;
+        } else {
+            console.error('Error getting PDF data from the server:', response.status);
+        }
+    } catch (error) {
         console.error('Error:', error);
-    });
+    }
 }
+
+// Assuming you have a button with id="pdf-file-input" in your HTML
+document.getElementById('pdf-file-input').addEventListener('change', async function (event) {
+    const file = event.target.files[0];
+
+    try {
+        const jsonData = await pdfToJson(file);
+        await sendToServer(jsonData);
+    } catch (error) {
+        console.error('An error occurred:', error);
+    }
+});
