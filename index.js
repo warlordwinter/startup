a1457766a0ec9ddd2905b881a7e0e264c55d18c6
Wiley Welch <wileyjwelch1@gmail.com>
2023-11-18 18:21:01 -0700

    Fixed the index and other stuff.

--- index.js
+++ index.js
@@ -1,154 +1,134 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
+const express = require('express')
+const cookieParser = require('cookie-parser')
 // const bodyParser = require('body-parser');
-const app = express();
-const mongo = require('./mongo');
-// const multer = require('multer');
-const { client, addPDF } = require('./mongo');
+const app = express()
+const {
+  client,
+  addPDF,
+  validateLogin,
+  getUser,
+  addUser,
+  getPDF,
+} = require('./mongo')
 
-app.use(cookieParser());
-app.use(express.json());
-app.use(express.static('public'));
+const multer = require('multer')
+const upload = multer()
+// Looks like maybe you were already thinking about doing this? because you have an addPDF function here?
+// I was trying to earlier but it wasn't working right. So I was just tinkering around.
+//ok, lets see if we can get it to work
 
-app.post("/login", (req, res) => {
-    if (req.body.username === undefined || req.body.password === undefined) {
-        res.status(400).end();
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.static('public'))
+
+app.post('/login', (req, res) => {
+  if (req.body.username === undefined || req.body.password === undefined) {
+    res.status(400).end()
+  } else {
+    if (validateLogin(req.body.username, req.body.password)) {
+      res.status(200).json({ username: req.body.username })
+      // console.log(`User '${req.body.username}' successfully logged in`);
     } else {
-        if (mongo.validateLogin(req.body.username, req.body.password)) {
-            res.status(200).json({ username: req.body.username });
-            // console.log(`User '${req.body.username}' successfully logged in`);
-        } else {
-            console.log(`User '${req.body.username}' not found or incorrect password`);
-            res.status(400).json({ error: "Invalid credentials" });
-        }
+      console.log(`User '${req.body.username}' not found or incorrect password`)
+      res.status(400).json({ error: 'Invalid credentials' })
     }
-});
+  }
+})
 
-
-app.post("/signup", (req, res) => {
-    if (req.body.username === undefined || req.body.password === undefined) {
-        res.status(400).end();
+app.post('/signup', (req, res) => {
+  if (req.body.username === undefined || req.body.password === undefined) {
+    res.status(400).end()
+  } else {
+    if (getUser(req.body.username)) {
+      console.log('This username is already being used')
+      res.status(409).json({ error: 'Username already in use.' })
     } else {
-        if (mongo.getUser(req.body.username)) {
-            console.log("This username is already being used");
-            res.status(409).json({ error: "Username already in use." });
-        } else {
-            mongo.addUser(req.body.username, req.body.password);
-            res.status(201).json({ message: "User registered successfully." });
-        }
+      addUser(req.body.username, req.body.password)
+      res.status(201).json({ message: 'User registered successfully.' })
     }
-});
+  }
+})
 
-const apiKey = process.env.OPENAI_API_KEY;
+const apiKey = process.env.OPENAI_API_KEY
 app.post('/send-message', async (req, res) => {
-    const userMessage = req.body.message;
+  const userMessage = req.body.message
 
-    // Perform OpenAI API request using apiKey
-    try {
-        const response = await fetch('https://api.openai.com/v1/chat/completions', {
-            method: 'POST',
-            headers: {
-                'Authorization': `Bearer ${apiKey}`,
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                messages: [
-                    { role: 'system', content: 'You are a chatbot' },
-                    { role: 'user', content: userMessage },
-                ],
-            }),
-        });
+  // Perform OpenAI API request using apiKey
+  try {
+    const response = await fetch('https://api.openai.com/v1/chat/completions', {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        messages: [
+          { role: 'system', content: 'You are a chatbot' },
+          { role: 'user', content: userMessage },
+        ],
+      }),
+    })
 
-        if (response.ok) {
-            const data = await response.json();
-            res.json({ botResponse: data.choices[0].message.content });
-        } else {
-            res.status(response.status).json({ error: 'Error sending message to the API' });
-        }
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).json({ error: 'Internal server error' });
+    if (response.ok) {
+      const data = await response.json()
+      res.json({ botResponse: data.choices[0].message.content })
+    } else {
+      res
+        .status(response.status)
+        .json({ error: 'Error sending message to the API' })
     }
-});
-
-
-// index.js
-
-// index.js
-
-const multer = require('multer');
-const upload = multer();
-
-// index.js
-
-// index.js
+  } catch (error) {
+    console.error('Error:', error)
+    res.status(500).json({ error: 'Internal server error' })
+  }
+})
 
 app.post('/upload-pdf', upload.single('pdf'), async (req, res) => {
-    try {
-        console.log("uploading pdf:" ,req.file)
-      const fileData = req.file.buffer;
-  
-      const client = new MongoClient(url);
-      await client.connect();
-      const db = client.db('rental');
-      const pdfCollection = db.collection('pdfs');
-  
-      const result = await pdfCollection.insertOne({ data: fileData });
-  
-      if (result.insertedId) {
-        console.log(`PDF stored in MongoDB with _id: ${result.insertedId}`);
-        res.json({ success: true });
-      } else {
-        console.error('Failed to insert PDF into MongoDB:', result);
-        res.status(500).json({ success: false, error: 'Internal server error' });
-      }
-    } catch (ex) {
-      console.error('Error storing PDF in MongoDB:', ex);
-      res.status(500).json({ success: false, error: 'Internal server error' });
-    } finally {
-      await client.close();
+  try {
+    console.log('uploading pdf:', req.file)
+    const fileData = req.file.buffer
+    const result = await addPDF('newuser', fileData) // :)
+
+    if (result.insertedId) {
+      console.log(`PDF stored in MongoDB with _id: ${result.insertedId}`)
+      res.json({ success: true })
+    } else {
+      console.error('Failed to insert PDF into MongoDB:', result)
+      res.status(500).json({ success: false, error: 'Internal server error' })
     }
-  });
-  
-  
+  } catch (ex) {
+    console.error('Error storing PDF in MongoDB:', ex)
+    res.status(500).json({ success: false, error: 'Internal server error' })
+  }
+})
 
-  
-  
-
-
+//Notes from Gabe
+//Make sure to get the filename requested from the get request!!!
 app.get('/get-pdf', async (req, res) => {
-    try {
-        await client.connect();
-        const db = client.db('startup');
-        const pdfCollection = db.collection('pdfs');
+  try {
+    if (req.body.username === undefined || req.body.filename === undefined) {
+      res.status(400).end()
+    } else {
+      const result = await getPDF(req.body.username, req.body.filename)
 
-        const result = await pdfCollection.find().toArray();
-
-        console.log('Result from MongoDB:', result);  // Add this line for debugging
-
-        if (result.length > 0) {
-            const pdfData = result[0].data; // Assuming the data field contains the PDF content
-            res.setHeader('Content-Type', 'application/pdf');
-            res.send(pdfData);
-        } else {
-            console.log('No PDF found');
-            res.status(404).json({ success: false, error: 'No PDF found' });
-        }
-    } catch (ex) {
-        console.log(`Error retrieving PDF from MongoDB: ${ex.message}`);
-        res.status(500).json({ success: false, error: ex.message });
-    } finally {
-        await client.close();
+      if (result) {
+        console.log('We have the user and filename', result)
+        res.status(200).json({ result })
+      } else {
+        res.status(404).json({ error: 'PDF not found' })
+      }
     }
-});
-
-
+  } catch (ex) {
+    console.log(`Error retrieving PDF from MongoDB: ${ex.message}`)
+    res.status(500).json({ success: false, error: ex.message })
+  }
+})
 
 app.use((_req, res) => {
-    res.sendFile('index.html', { root: 'public' });
-});
+  res.sendFile('index.html', { root: 'public' })
+})
 
-const port = 4000;
+const port = 4000
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
+  console.log(`Server is running at http://localhost:${port}`)
+})
--- mongo.js
+++ mongo.js
@@ -3,85 +3,94 @@ module.exports = {
     addUser: addUser,
     getUser: getUser,
     validateLogin: validateLogin,
-    getPDFs: getPDFs,
+    getPDF: getPDF,
     addPDF: addPDF,
-
-}
-
-const { MongoClient } = require('mongodb');
-const config = require('./dbConfig.json');
-
-// const encodedPassword = encodeURIComponent(config.password);
-const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
-const client = new MongoClient(url);
-
-
-const db = client.db('startup');
-const userCollection = db.collection('users');
-const pdfCollection = db.collection('pdfs');
-
-(async function testConnection() {
-    await client.connect();
-    await db.command({ ping: 1 });
-    console.log("Connected successfully to server");
-})().catch((ex) => {
-    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
-    process.exit(1);
-});
-
-async function addPDF(username,data) {
-    const result = await pdfCollection.insertOne({ username: username, data: data });
-    console.log(`PDF added in MongoDB with username: ${result.username}`);
-    if (result) {
-        console.log("PDF added to MongoDB");
-        return true;
-    } else {
-        console.log("failed to add PDF to MongoDB");
-        return false;
-    }
-}
-
-
-async function addUser(username, password) {
+  }
+  
+  const { MongoClient } = require('mongodb')
+  const config = require('./dbConfig.json')
+  
+  // const encodedPassword = encodeURIComponent(config.password);
+  const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`
+  const client = new MongoClient(url)
+  
+  ;(async function testConnection() {
+    await client.connect()
+    await db.command({ ping: 1 })
+    console.log('Connected successfully to server')
+  })().catch((ex) => {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`)
+    process.exit(1)
+  })
+  
+  const db = client.db('startup')
+  const userCollection = db.collection('users')
+  const pdfCollection = db.collection('pdfs')
+  
+  async function addPDF(username, data) {
     try {
-        const result = await userCollection.insertOne({ username: username, password: password });
-        console.log(`User added in MongoDB with _id: ${result.insertedId}`);
+      const result = await pdfCollection.insertOne({
+        username: username,
+        data: data,
+      })
+      console.log('result', result)
+      return result
+    } catch (error) {
+      console.log('failed to add PDF to MongoDB', error)
+      return false
+    }
+  }
+  
+  async function getPDF(username, fileName) {
+    try {
+      const result = await pdfCollection.find({
+        username: 'username',
+        fileName: 'filename',
+      })
+      return result
+    } catch (error) {
+      console.error('error getting the PDF', error)
+      return false
+    }
+    console.log('Result from MongoDB:', result) // Add this line for debugging
+  }
+  
+  async function addUser(username, password) {
+    try {
+      const result = await userCollection.insertOne({
+        username: username,
+        password: password,
+      })
+      console.log(`User added in MongoDB with _id: ${result.insertedId}`)
     } catch (ex) {
-        console.log(`Error storing user in MongoDB: ${ex.message}`);
+      console.log(`Error storing user in MongoDB: ${ex.message}`)
     } finally {
-        await client.close();
+      await client.close()
     }
-}
-
-async function getUser(username) {
-    const result = await userCollection.findOne({ username: username });
+  }
+  
+  async function getUser(username) {
+    const result = await userCollection.findOne({ username: username })
     if (result) {
-        console.log("User found");
-        return true;
+      console.log('User found')
+      return true
     } else {
-        console.log("User not found");
-        return false;
+      console.log('User not found')
+      return false
     }
-}
-
-async function validateLogin(username,password){
-    const result = await userCollection.findOne({ 'username': username, 'password' : password});
+  }
+  
+  async function validateLogin(username, password) {
+    const result = await userCollection.findOne({
+      username: username,
+      password: password,
+    })
     if (result) {
-        console.log("User found");
-        return true;
+      console.log('User found')
+      return true
     } else {
-        console.log("User not found");
-        return false;
+      console.log('User not found')
+      return false
     }
-}
-
-async function getPDFs(username) {
-    const result = await pdfCollection.findOne({ username: username });
-    if (result) {
-        console.log("We found the PDFs")
-        return result['pdf'];
-    } else {
-        console.log("user not found (getPDFs)");
-        return null;
-    }
-}
+  }
+  
